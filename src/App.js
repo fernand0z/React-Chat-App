33ea18b8ae505639fc48cf393585d11b16b6386c
Fernando <fernandoz@outlook.com>
2018-07-18 18:40:04 -0700

    Added containers and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Sidebar } from "./containers/Sidebar";
+import { MessagesList } from "./containers/MessagesList";
+import { AddMessage } from "./containers/AddMessage";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div id='container'>
-        <aside id='sidebar'>Users</aside>
-          <section id='main'>
-            <section id='messages-list'>Messages List</section>
-            <section id='new-message'>New Message</section>
-          </section>
+      <div id="container">
+        <Sidebar />
+        <section id="main">
+          <MessagesList />
+          <AddMessage />
+        </section>
       </div>
     );
   }
